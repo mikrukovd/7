9a854bb150b717147fdb42f29b5cf975404e6916
mikrukovd <z123456689z@gmail.com>
2024-12-25 14:46:26 +0300

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,29 +1,8 @@
 import ptbot
 import os
+
 from pytimeparse import parse
 from dotenv import load_dotenv
-
-
-load_dotenv()
-TG_TOKEN = os.getenv('TOKEN_TELEGA')
-BOT = ptbot.Bot(TG_TOKEN)
-
-
-def reply(chat_id, message):
-    time = parse(message)
-    text = 'Осталось {} секунд\n{}'.format(time, render_progressbar(time, 0))
-    message_id = BOT.send_message(chat_id, text)
-    BOT.create_countdown(time, notify_progress, message_id=message_id, chat_id=chat_id, time=time)
-    BOT.create_timer(time, answer, chat_id=chat_id)
-
-
-def notify_progress(secs_left, chat_id, message_id, time):
-    text = 'Осталось {} секунд\n{}'.format(secs_left, render_progressbar(time, time-secs_left))
-    BOT.update_message(chat_id, message_id, text)
-
-
-def answer(chat_id):
-    BOT.send_message(chat_id, 'Время вышло')
 
 
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
@@ -36,8 +15,26 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
 
 
 def main():
-    BOT.reply_on_message(reply)
-    BOT.run_bot()
+    load_dotenv()
+    tg_token = os.getenv('TOKEN_TELEGA')
+    bot = ptbot.Bot(tg_token)
+
+    def answer(chat_id):
+        bot.send_message(chat_id, 'Время вышло')
+
+    def notify_progress(secs_left, chat_id, message_id, time):
+        text = 'Осталось {} секунд\n{}'.format(secs_left, render_progressbar(time, time-secs_left))
+        bot.update_message(chat_id, message_id, text)
+
+    def reply(chat_id, message):
+        time = parse(message)
+        text = 'Осталось {} секунд\n{}'.format(time, render_progressbar(time, 0))
+        message_id = bot.send_message(chat_id, text)
+        bot.create_countdown(time, notify_progress, message_id=message_id, chat_id=chat_id, time=time)
+        bot.create_timer(time, answer, chat_id=chat_id)
+
+    bot.reply_on_message(reply)
+    bot.run_bot()
 
 
 if __name__ == '__main__':
