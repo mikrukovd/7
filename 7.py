08f11f6ecab9589c39d8aa38e92df7841e9202c3
mikrukovd <z123456689z@gmail.com>
2024-12-28 08:11:08 +0300

    Update 7.py

--- 7.py
+++ 7.py
@@ -5,34 +5,38 @@ from pytimeparse import parse
 from dotenv import load_dotenv
 
 
+def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
+    iteration = min(total, iteration)
+    percent = "{0:.1f}"
+    percent = percent.format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    pbar = fill * filled_length + zfill * (length - filled_length)
+    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
+
+
+def timeout(chat_id, bot):
+    text = 'Время вышло'
+    bot.send_message(chat_id, text)
+
+
+def notify_progress(secs_left, chat_id, bot, message_id, time):
+    text = 'Осталось {} секунд\n{}'.format(secs_left, render_progressbar(time, time-secs_left))
+    bot.update_message(chat_id, message_id, text)
+
+
+def reply(author_id, author_message, bot):
+    time = parse(author_message)
+    text = 'Осталось {} секунд\n{}'.format(time, render_progressbar(time, 0))
+    message_id = bot.send_message(author_id, text)
+    bot.create_countdown(time, notify_progress, chat_id=author_id, bot=bot, message_id=message_id, time=time)
+    bot.create_timer(time, timeout, chat_id=author_id, bot=bot)
+
+
 def main():
     load_dotenv()
-    tg_token = os.getenv('TOKEN_TELEGA')
-    bot = ptbot.Bot(tg_token)
-
-    def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
-        iteration = min(total, iteration)
-        percent = "{0:.1f}"
-        percent = percent.format(100 * (iteration / float(total)))
-        filled_length = int(length * iteration // total)
-        pbar = fill * filled_length + zfill * (length - filled_length)
-        return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
-
-    def answer(chat_id):
-        bot.send_message(chat_id, 'Время вышло')
-
-    def notify_progress(secs_left, chat_id, message_id, time):
-        text = ('Осталось {} секунд\n{}').format(secs_left, render_progressbar(time, time-secs_left))
-        bot.update_message(chat_id, message_id, text)
-
-    def reply(chat_id, message):
-        time = parse(message)
-        text = ('Осталось {} секунд\n{}').format(time, render_progressbar(time, 0))
-        message_id = bot.send_message(chat_id, text)
-        bot.create_countdown(time, notify_progress, message_id=message_id, chat_id=chat_id, time=time)
-        bot.create_timer(time, answer, chat_id=chat_id)
-
-    bot.reply_on_message(reply)
+    TG_TOKEN = os.getenv('TOKEN_TELEGA')
+    bot = ptbot.Bot(TG_TOKEN)
+    bot.reply_on_message(reply, bot=bot)
     bot.run_bot()
 
 
